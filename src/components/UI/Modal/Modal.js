c5d3a92fc377a802d3763c505fc785c72c58dc3a
kamal11 <kyzx11@gmail.com>
2018-11-16 16:22:31 +0530

    placed backdrop beside Modal

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,18 +1,22 @@
 import React from "react";
 
 import classes from "./Modal.css";
+import Backdrop from "../BackDrop/Backdrop";
 
 const modal = props => {
   return (
-    <div
-      className={classes.Modal}
-      style={{
-        transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-        opacity: props.show ? "1" : "0"
-      }}
-    >
-      {props.children}
-    </div>
+    <React.Fragment>
+      <Backdrop show={props.show} />
+      <div
+        className={classes.Modal}
+        style={{
+          transform: props.show ? "translateY(0)" : "translateY(-100vh)",
+          opacity: props.show ? "1" : "0"
+        }}
+      >
+        {props.children}
+      </div>
+    </React.Fragment>
   );
 };
 
