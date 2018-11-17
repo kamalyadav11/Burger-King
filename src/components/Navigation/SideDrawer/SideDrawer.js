84e2ae697e8f544ba9eae224aa7e20e0f865baca
kamal11 <kyzx11@gmail.com>
2018-11-17 22:38:11 +0530

    rendered sidedrawer dynamically

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -7,10 +7,15 @@ import NavigationItems from "../NavigationItems/NavigationItems";
 import classes from "./SideDrawer.css";
 
 const sideDrawer = props => {
+  let attachedClasses = [classes.SideDrawer, classes.Close];
+  if (props.open) {
+    attachedClasses = [classes.SideDrawer, classes.Open];
+  }
+
   return (
     <React.Fragment>
       <Backdrop show={props.open} clicked={props.closed} />
-      <div className={classes.SideDrawer}>
+      <div className={attachedClasses.join(" ")}>
         <Logo height="10%" marginBottom="32px" />
         <nav>
           <NavigationItems />
