c217a23a4fe43a86cea83df3ef3282b167d84e6b
kamal11 <kyzx11@gmail.com>
2018-11-17 22:21:43 +0530

    changed layout to stateful component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,14 +4,16 @@ import classes from "./Layout.css";
 import Toolbar from "../Navigation/ToolBar/ToolBar";
 import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-const Layout = props => {
-  return (
-    <React.Fragment>
-      <SideDrawer />
-      <Toolbar />
-      <main className={classes.Content}>{props.children}</main>
-    </React.Fragment>
-  );
-};
+class Layout extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <SideDrawer />
+        <Toolbar />
+        <main className={classes.Content}>{this.props.children}</main>
+      </React.Fragment>
+    );
+  }
+}
 
 export default Layout;
