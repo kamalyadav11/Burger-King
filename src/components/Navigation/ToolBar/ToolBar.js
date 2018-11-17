4a768ffe8af230fc21562e6cb6f4c9418086044e
kamal11 <kyzx11@gmail.com>
2018-11-17 22:04:01 +0530

    typo fix

--- src/components/Navigation/ToolBar/ToolBar.js
+++ src/components/Navigation/ToolBar/ToolBar.js
@@ -4,7 +4,7 @@ import classes from "./ToolBar.css";
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
 
-const toolbar = props => {
+const toolbar = () => {
   return (
     <header className={classes.Toolbar}>
       <div>MENU</div>
