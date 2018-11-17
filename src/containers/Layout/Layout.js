04ffc9381af823ca59bfa4699539970fddb7059e
kamal11 <kyzx11@gmail.com>
2018-11-17 23:20:59 +0530

    improved project folder structure

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 import classes from "./Layout.css";
-import Toolbar from "../Navigation/ToolBar/ToolBar";
-import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
+import Toolbar from "../../components/Navigation/ToolBar/ToolBar";
+import SideDrawer from "../../components/Navigation/SideDrawer/SideDrawer";
 
 class Layout extends React.Component {
   state = {
