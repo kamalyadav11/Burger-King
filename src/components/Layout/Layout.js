71c5887716157ae3112ae5edc160806426ff8f55
kamal11 <kyzx11@gmail.com>
2018-11-17 21:43:13 +0530

    rendered sideDrawer on Layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,11 +2,12 @@ import React from "react";
 
 import classes from "./Layout.css";
 import Toolbar from "../Navigation/ToolBar/ToolBar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 const Layout = props => {
   return (
     <React.Fragment>
-      {/* <div>Toolbar, SideDrawer, Backdrop</div> */}
+      <SideDrawer />
       <Toolbar />
       <main className={classes.Content}>{props.children}</main>
     </React.Fragment>
