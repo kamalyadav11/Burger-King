3ba846eb94089de1993331955a737f5bc6bd4d24
kamal11 <kyzx11@gmail.com>
2018-11-22 21:20:25 +0530

    added axios interceptord

--- src/hoc/withErrorhandler/withErrorHandler.js
+++ src/hoc/withErrorhandler/withErrorHandler.js
@@ -3,10 +3,24 @@ import Modal from "../../components/UI/Modal/Modal";
 
 const withErrorHandler = (WrappedComponent, axios) => {
   return class extends React.Component {
+    state = {
+      error: null
+    };
+
+    componentDidMount() {
+      axios.interceptors.request.use(req => {
+        this.setState({ error: null });
+      });
+
+      axios.interceptors.response.use(null, error => {
+        this.setState({ error: error });
+      });
+    }
+
     render() {
       return (
         <React.Fragment>
-          <Modal show>Something went Wrong!!!</Modal>
+          <Modal show={this.state.error}>{this.state.error.message}</Modal>
           <WrappedComponent {...this.props} />
         </React.Fragment>
       );
