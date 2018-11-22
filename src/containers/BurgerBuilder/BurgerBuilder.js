1e36c60734e73e5c73693c65bc499cd248a05923
kamal11 <kyzx11@gmail.com>
2018-11-22 21:46:25 +0530

    changed did to will so that all errors(if any) are showed

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -25,7 +25,7 @@ class BurgerBuilder extends Component {
 
   componentDidMount() {
     axios
-      .get("https://burgerking-54612.firebaseio.com/ingredients.json")
+      .get("https://burgerking-54612.firebaseio.com/ingredients")
       .then(response => this.setState({ ingredients: response.data }));
   }
 
