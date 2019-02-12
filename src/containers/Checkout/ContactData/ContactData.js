bd646dd18c4c6f23c0b7eefb5ef68c4b07e530d4
kamal11 <kyzx11@gmail.com>
2019-02-12 21:33:52 +0530

    added some css to contact data

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Button from "../../../components/UI/Button/Button";
+import classes from "./ContactData.css";
 
 export default class ContactData extends Component {
   state = {
@@ -13,7 +14,7 @@ export default class ContactData extends Component {
 
   render() {
     return (
-      <div>
+      <div className={classes.ContactData}>
         <h4>Enter your contact Data</h4>
         <form action="">
           <input type="text" name="name" placeholder="Your Name" />
