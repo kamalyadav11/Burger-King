62b3b4e7baf1b482ef45568e0ad41815d9175697
kamal11 <kyzx11@gmail.com>
2019-02-12 20:01:49 +0530

    added props to button

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -10,10 +10,10 @@ const CheckoutSummary = props => {
       <div style={{ width: "100%", margin: "auto" }}>
         <Burger ingredients={props.ingredients} />
       </div>
-      <Button btnType="Danger" clicked>
+      <Button btnType="Danger" clicked={props.onCheckoutCancelled}>
         CANCEL
       </Button>
-      <Button btnType="Success" clicked>
+      <Button btnType="Success" clicked={props.onCheckoutContinued}>
         CONTINUE
       </Button>
     </div>
