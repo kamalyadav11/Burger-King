59b4feaf4f373ff5d768c1731eaa77221ad587c6
kamal11 <kyzx11@gmail.com>
2019-02-12 19:27:51 +0530

    Added Burger and Button Component in CheckoutSummary

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -1,13 +1,20 @@
 import React from "react";
 import Burger from "../../Burger/Burger";
+import Button from "../../UI/Button/Button";
 
 const CheckoutSummary = props => {
   return (
     <div>
       <h1>We hope it tastes well!</h1>
       <div style={{ width: "300px", height: "300px", margin: "auto" }}>
-        <Burger />
+        <Burger ingredients={props.ingredients} />
       </div>
+      <Button btnType="Danger" clicked>
+        CANCEL
+      </Button>
+      <Button btnType="Success" clicked>
+        CONTINUE
+      </Button>
     </div>
   );
 };
