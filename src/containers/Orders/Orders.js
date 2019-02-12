76b3c8ee2482f891fbace385a0ec0a479f5bab0b
kamal11 <kyzx11@gmail.com>
2019-02-12 22:24:46 +0530

    fetching orders from server

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,7 +1,28 @@
 import React, { Component } from "react";
+import axios from "../../axios-orders";
 import Order from "../../components/Order/Order";
 
 export default class Orders extends Component {
+  state = {
+    orders: [],
+    loading: true
+  };
+
+  componentDidMount() {
+    axios
+      .get("orders.json")
+      .then(res => {
+        const fetchedOrders = [];
+        for (let key in res.data) {
+          fetchedOrders.push({ ...res.data[key], id: key });
+        }
+        this.setState({ loading: false, orders: fetchedOrders });
+      })
+      .catch(() => {
+        this.setState({ loading: false });
+      });
+  }
+
   render() {
     return (
       <div>
