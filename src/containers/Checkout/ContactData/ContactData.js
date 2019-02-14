c593b52dd9cfafd0bf203a8d231dc8f8744e5d86
kamal11 <kyzx11@gmail.com>
2019-02-14 15:40:19 +0530

    replaced input with Input component

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -3,6 +3,7 @@ import Button from "../../../components/UI/Button/Button";
 import classes from "./ContactData.css";
 import axios from "../../../axios-orders";
 import Spinner from "../../../components/UI/Spinner/Spinner";
+import Input from "../../../components/UI/Input/Input";
 
 export default class ContactData extends Component {
   state = {
@@ -44,30 +45,10 @@ export default class ContactData extends Component {
   render() {
     let form = (
       <form>
-        <input
-          className={classes.Input}
-          type="text"
-          name="name"
-          placeholder="Your Name"
-        />
-        <input
-          className={classes.Input}
-          type="email"
-          name="email"
-          placeholder="Your email"
-        />
-        <input
-          className={classes.Input}
-          type="text"
-          name="street"
-          placeholder="Street"
-        />
-        <input
-          className={classes.Input}
-          type="text"
-          name="postal"
-          placeholder="Postal Code"
-        />
+        <Input type="text" name="name" placeholder="Your Name" />
+        <Input type="email" name="email" placeholder="Your email" />
+        <Input type="text" name="street" placeholder="Street" />
+        <Input type="text" name="postal" placeholder="Postal Code" />
         <Button btnType="Success" clicked={this.orderHandler}>
           ORDER
         </Button>
