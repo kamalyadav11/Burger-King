3f25b2da73a766b10d319ed988ee2e3c24527e56
kamal11 <kyzx11@gmail.com>
2019-02-14 16:30:52 +0530

    rendered Select component

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -4,7 +4,6 @@ import classes from "./ContactData.css";
 import axios from "../../../axios-orders";
 import Spinner from "../../../components/UI/Spinner/Spinner";
 import Input from "../../../components/UI/Input/Input";
-import { format } from "url";
 
 export default class ContactData extends Component {
   state = {
