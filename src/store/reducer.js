23dfd4da26c9f71783752d4797489e6112f1b873
kamal11 <kyzx11@gmail.com>
2019-03-02 12:48:56 +0530

    store setup

---
+++ src/store/reducer.js
@@ -0,0 +1,10 @@
+import * as actionTypes from "./actions";
+
+const initialState = {
+  ingredients: null,
+  totalPrice: 5
+};
+
+const reducer = (state = initialState, action) => {};
+
+export default reducer;
